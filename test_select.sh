4f16afcfe80a1bdc5d29a3d211261d077144d53a
root <root@node2.example.com>
2018-12-10 14:28:37 +0530

    This is my third commit. I am using case in this commit.

--- test_select.sh
+++ test_select.sh
@@ -2,6 +2,18 @@
 PS3="HELLO USER KINDLY SELECT ANY ONE "number" WHICH YOU LIKE FOR DRINK: "
 select i in tea coffee Hot_water milk fruity
 do 
-echo "you have selected $i. Thanks for choosing $i. you will get $i ASAP."
+#echo "you have selected $i. Thanks for choosing $i. you will get $i ASAP."
+case $i in 
+tea)
+echo "you have selected tea. Thanks for choosing tea. you will get tea ASAP.";;
+coffee)
+echo "you have selected coffee. Thanks for choosing coffee. you will get coffee ASAP.";;
+Hot_water)
+echo "you have selected Hot_water. Thanks for choosing Hot_water. you will get Hot_water ASAP.";;
+milk)
+echo "you have selected milk. Thanks for choosing milk. you will get milk ASAP.";;
+fruity)
+echo "you have selected fruity. Thanks for choosing fruity. you will get fruity ASAP.";;
+esac
 break
 done
