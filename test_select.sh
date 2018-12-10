39f31cdc3f37be1dc2f7e69782752a054cd69964
Ganesh chandra joshi <37043894+joshi-git-hub@users.noreply.github.com>
2018-12-10 09:04:35 +0530

    PLEASE ADD MY COMMIT

--- test_select.sh
+++ test_select.sh
@@ -4,3 +4,13 @@ do
 echo "you have selected $i. Thanks for choosing $i. you will get $i ASAP."
 break
 done
+=======================
+=================================================result after run this script
+sh test_select.sh
+[root@node2 loop_scripting]# sh test_select.sh 
+1) tea
+2) coffee
+3) Hot_water
+4) milk
+5) fruity
+HELLO USER KINDLY SELECT ANY ONE number WHICH YOU LIKE FOR DRINK:
