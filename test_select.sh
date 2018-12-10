47235f02c69f5952d5e7c3e1e6fb5c1d30809b45
root <root@node2.example.com>
2018-12-10 14:17:58 +0530

    this is my second commit.

--- test_select.sh
+++ test_select.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PS3="HELLO USER KINDLY SELECT ANY ONE "number" WHICH YOU LIKE FOR DRINK: "
 select i in tea coffee Hot_water milk fruity
 do 
 echo "you have selected $i. Thanks for choosing $i. you will get $i ASAP."
